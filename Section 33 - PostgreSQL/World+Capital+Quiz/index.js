8d57dfd453f948dcce196a2b8ce33233b8b02403
mon0theist <bburns91@gmail.com>
2026-04-15 16:26:25 -0500

    READ data from a Postgres database. Need to complete Flags exercise

--- Section 33 - PostgreSQL/World+Capital+Quiz/index.js
+++ Section 33 - PostgreSQL/World+Capital+Quiz/index.js
@@ -1,14 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "postgres",
+  port: 5432,
+});
+
+db.connect();
+
+// technically this doesn't have to be commented out
+// since it gets overwritten later in the db.query
+// let quiz = [
+//   { country: "France", capital: "Paris" },
+//   { country: "United Kingdom", capital: "London" },
+//   { country: "United States of America", capital: "New York" },
+// ];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  }
+  else {
+    let quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
