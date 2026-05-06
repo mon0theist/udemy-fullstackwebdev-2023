6791ba5a69a7b8ba57f77c9b66e4e97d53b94d60
mon0theist <bburns91@gmail.com>
2026-05-06 16:04:08 -0500

    Book Notes Capstone project (in progress)

--- Section 34 - Capstone Project - Book Notes/index.js
+++ Section 34 - Capstone Project - Book Notes/index.js
@@ -43,19 +43,54 @@ app.get("/", async (req, res) => {
     });
 });
 
+
 // add new book
-app.post("/add", async (req, res) => {
-    const title = req.body.newBookTitle;
-    const author = req.body.newBookAuthor;
-    console.log(title, author);
-    res.redirect("/");
+app.get("/new", async (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/add", async (req, res) => {    
+    try {
+        const title = req.body.newBookTitle.trim().replaceAll(" ", "+");
+        const author = req.body.newBookAuthor.trim().replaceAll(" ", "+");
+        const api_response = await fetch(`https://openlibrary.org/search.json?title=${title}&author=${author}`);
+        const data = await api_response.json();
+        
+        if (data.numFound > 0){
+            const ol_id = data.docs[0].cover_edition_key;
+            const coverImgURL = `https://covers.openlibrary.org/b/olid/${ol_id}-M.jpg`;
+            const titlename = data.docs[0].title;
+            const authorname = data.docs[0].author_name[0]; //author_name is an array for some reason
+            const yrpub = data.docs[0].first_publish_year;
+            const rating = req.body.rating;
+            const yrread = req.body.yrread;
+            const summary = req.body.summary;
+            
+            const query = await db.query(`
+                INSERT INTO finished_books (ol_id, title, author, yr_pub, yr_read, rating, summary, cover_url)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+                [ol_id, titlename, authorname, yrpub, yrread, rating, summary, coverImgURL]
+            );
+            
+            console.log(`${ol_id} successfully added to database`)
+            res.redirect("/");
+        }
+        else {
+            console.log("Error: No search results found");
+            res.redirect("/");
+        };
+    }
+    catch (err) {
+        console.log(err);
+        res.redirect("/");
+    }
 });
 
 // view/edit book info
 app.get("/edit", async (req, res) => {
     const books = await getBooks();
-    // console.log(books);
     const book = books[0];
+
     res.render("edit.ejs", {
         book: book,
     });
